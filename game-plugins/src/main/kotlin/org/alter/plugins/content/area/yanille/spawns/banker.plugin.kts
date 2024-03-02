b7f0dc5e2c6db92f1bc2357e1ffd94a9240d016a
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-03-02 21:28:16 +0100

    Created Yanille Spawns

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/yanille/spawns/banker.plugin.kts
@@ -0,0 +1,5 @@
+package org.alter.plugins.content.area.yanille.spawns
+
+spawn_npc(Npcs.BANKER_1613, 2615, 3094, 0, 0, Direction.WEST)
+spawn_npc(Npcs.BANKER_1634, 2615, 3092, 0, 0, Direction.WEST)
+spawn_npc(Npcs.BANKER_1613, 2615, 3091, 0, 0, Direction.WEST)
---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/yanille/spawns/spawns.plugin.kts
@@ -0,0 +1,51 @@
+package org.alter.plugins.content.area.yanille.spawns
+
+spawn_npc(Npcs.MAKEOVER_MAGE, 2603, 3088, 0, 2, Direction.EAST)
+
+spawn_npc(Npcs.ARCHMAGE_SEDRIDOR_11433, 2591, 3086, 0, 5, Direction.EAST)
+
+spawn_npc(Npcs.GIANT_RAT_2858, 2599, 3104, 0, 2, Direction.SOUTH)
+spawn_npc(Npcs.GIANT_RAT_2858, 2602, 3102, 0, 2, Direction.SOUTH)
+spawn_npc(Npcs.GIANT_RAT_2858, 2603, 3106, 0, 2, Direction.SOUTH)
+spawn_npc(Npcs.GIANT_RAT_2858, 2609, 3105, 0, 2, Direction.SOUTH)
+spawn_npc(Npcs.GIANT_RAT_2858, 2609, 3103, 0, 2, Direction.SOUTH)
+
+spawn_npc(Npcs.COW, 2606, 3113, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.COW, 2602, 3113, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.COW, 2603, 3115, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.COW, 2606, 3118, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.COW, 2602, 3118, 0, 3, Direction.NORTH)
+
+spawn_npc(Npcs.SHOP_KEEPER, 2595, 3105, 0, 2, Direction.SOUTH)
+spawn_npc(Npcs.AUBURY_11434, 2594, 3105, 0, 2, Direction.SOUTH)
+
+spawn_npc(Npcs.WATCHMAN_5420, 2585, 3099, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.WATCHMAN_5420, 2582, 3099, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.WATCHMAN_5420, 2579, 3099, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.WATCHMAN_5420, 2585, 3096, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.WATCHMAN_5420, 2582, 3096, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.WATCHMAN_5420, 2579, 3096, 0, 5, Direction.SOUTH)
+
+spawn_npc(Npcs.FARMER_3114, 2588, 3104, 0, 3, Direction.SOUTH)
+spawn_npc(Npcs.MASTER_FARMER_5730, 2578, 3101, 0, 3, Direction.SOUTH)
+
+spawn_npc(Npcs.MOSS_GIANT, 2580, 3076, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.MOSS_GIANT, 2574, 3078, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.MOSS_GIANT, 2573, 3083, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.MOSS_GIANT, 2576, 3084, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.MOSS_GIANT, 2580, 3081, 0, 3, Direction.NORTH)
+
+spawn_npc(Npcs.HERO_3295, 2540, 3089, 0, 3, Direction.EAST)
+spawn_npc(Npcs.HERO_3295, 2540, 3093, 0, 3, Direction.EAST)
+spawn_npc(Npcs.HERO_3295, 2543, 3093, 0, 3, Direction.EAST)
+spawn_npc(Npcs.HERO_3295, 2543, 3089, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.DAD_6391, 2544, 3095, 0, 3, Direction.SOUTH)
+
+spawn_npc(Npcs.BATTLE_MAGE_1611, 2628, 3083, 0, 5, Direction.NORTH)
+spawn_npc(Npcs.BATTLE_MAGE_1611, 2624, 3083, 0, 5, Direction.NORTH)
+spawn_npc(Npcs.BATTLE_MAGE_1611, 2624, 3079, 0, 5, Direction.NORTH)
+spawn_npc(Npcs.BATTLE_MAGE_1611, 2624, 3075, 0, 5, Direction.NORTH)
+spawn_npc(Npcs.BATTLE_MAGE_1611, 2629, 3077, 0, 5, Direction.NORTH)
+
+spawn_npc(Npcs.ARZINIAN_AVATAR_OF_MAGIC_1235, 2627, 3079, 0, 5, Direction.NORTH)
