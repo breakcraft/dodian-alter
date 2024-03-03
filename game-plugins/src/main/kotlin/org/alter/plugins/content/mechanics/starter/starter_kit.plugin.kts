975cbf68eeb9b15b2c06f086de8a5f06acd2f406
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-03-03 22:08:39 +0100

    Update Starter

--- game-plugins/src/main/kotlin/org/alter/plugins/content/mechanics/starter/starter_kit.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/mechanics/starter/starter_kit.plugin.kts
@@ -5,12 +5,15 @@ import org.alter.game.model.attr.NEW_ACCOUNT_ATTR
 on_login {
     if (player.attr[NEW_ACCOUNT_ATTR] ?: return@on_login) {
         with (player.inventory) {
-            add(Items.LOGS, 5)
-            add(Items.TINDERBOX)
             add(Items.BREAD, 5)
             add(Items.BRONZE_PICKAXE)
-            add(Items.BRONZE_DAGGER)
-            add(Items.KNIFE)
+            add(Items.BRONZE_AXE)
+            add(Items.TRAINING_SWORD)
+            add(Items.TRAINING_SHIELD)
+            add(Items.TRAINING_BOW)
+            add(Items.TRAINING_ARROWS, 30)
+            add(Items.AIR_RUNE, 30)
+            add(Items.MIND_RUNE, 30)
         }
     }
 }
