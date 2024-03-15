fc0da3a2e16fa9efc48c3dd2a0288db673c5e5a1
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-03-15 18:36:57 +0100

    Update Altar_spawn.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/skills/runecrafting/Altar_spawn.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/skills/runecrafting/Altar_spawn.plugin.kts
@@ -5,5 +5,5 @@ package org.alter.plugins.content.skills.runecrafting
  */
 //Spawn runecraft altars as old fashion Dodian
 world.spawn(DynamicObject(id = Objs.ALTAR_34768, type = 10, rot = 0, tile = Tile(2627, 3116, 0)))//Nature altar
-world.spawn(DynamicObject(id = Objs.BLOOD_ALTAR, type = 10, rot = 0, tile = Tile(2688, 3474, 0)))//Blood altar
+world.spawn(DynamicObject(id = Objs.BLOOD_ALTAR, type = 10, rot = 1, tile = Tile(2686, 3472, 0)))//Blood altar
 
