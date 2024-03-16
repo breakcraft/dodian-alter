6f435c6669dbb0e7a80e7dd6a6453b72c5af0e61
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-03-16 17:10:20 +0100

    Add West ardy tele command

--- game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/player/teleports_player.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/player/teleports_player.plugin.kts
@@ -16,6 +16,9 @@ on_command("edge", description = "Teleports you to Edgeville") {
 on_command("ardy", description = "Teleports you to East Ardougne") {
     player.moveTo(Tile(x = 2662, z = 3307, height = 0))
 }
+on_command("wardy", description = "Teleports you to West Ardougne") {
+    player.moveTo(Tile(x = 2662, z = 3307, height = 0))
+}
 on_command("seers", description = "Teleports you to Seers Village") {
     player.moveTo(Tile(x = 2725, z = 3484, height = 0))
 }
