2f642e4c92c814e15cf3d8acb07e634fd03858a6
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-03-17 13:15:50 +0100

    Add robe exit
    need to spawn smokey well as entrance for it at the fishing guild (id: 6279)

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/smokedungeon/smokedungeon.plugin.kts
@@ -0,0 +1,10 @@
+package org.alter.plugins.content.area.smokedungeon
+
+/**
+ * @author Eikenb00m <https://github.com/eikenb00m>
+ */
+
+on_obj_option(obj = Objs.ROPE_6439, option = "Climb-Up") {
+    player.moveTo(2596, 3409)
+}
+
